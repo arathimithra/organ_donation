8cc754b8349fe522e2803c49de6b780ac2f4f72e
arathimithra <arayhi.ams@gmail.com>
2023-07-21 15:12:17 +0530

    n

--- odm_app/models.py
+++ odm_app/models.py
@@ -19,3 +19,17 @@ class Donor(models.Model):
 
     def __str__(self):
         return self.name
+
+class BloodDonor(models.Model):
+
+    name = models.CharField(max_length=500)
+    age = models.CharField(max_length=100)
+    gender =models.CharField(max_length=100)
+    blood_group = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    status = models.BooleanField()
+
+    def __str__(self):
+        return self.name
